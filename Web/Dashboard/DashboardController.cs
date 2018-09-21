e871dca8327b5ee3c15cd89d98415947765f304e
György Balássy <balassy@users.noreply.github.com>
2018-09-21 17:15:41 +0200

    ADDED: Dummy API endpoint.

---
+++ Web/Dashboard/DashboardController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using BuildMonitor.Web.Dashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildMonitor.Web.Dashboard
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  [Produces("application/json")]
+  public class DashboardController : ControllerBase
+  {
+    [HttpGet("{id}", Name = "Get")]
+    [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Required by the runtime.")]
+    public DashboardResult Get(string id)
+    {
+      DashboardResult result = new DashboardResult
+      {
+        Id = id,
+        Title = "My Application",
+        Groups = new List<BuildResultGroup>
+        {
+          new BuildResultGroup
+          {
+            Title = "Frontend builds",
+            Builds = new List<BuildResult>
+            {
+              new BuildResult { Title = "Component A" },
+              new BuildResult { Title = "Component B" },
+              new BuildResult { Title = "Component C" },
+            }
+          },
+          new BuildResultGroup
+          {
+            Title = "Backend builds",
+            Builds = new List<BuildResult>
+            {
+              new BuildResult { Title = "Service A" },
+              new BuildResult { Title = "Service B" },
+              new BuildResult { Title = "Service C" },
+            }
+          }
+        }
+      };
+
+      return result;
+    }
+  }
+}
---
+++ Web/Dashboard/Models/DashboardResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BuildMonitor.Web.Dashboard.Models
+{
+  public class DashboardResult
+  {
+    public string Id { get; set; }
+
+    public string Title { get; set; }
+
+    [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "Required for JSON serialization.")]
+    public List<BuildResultGroup> Groups { get; set; }
+  }
+}
