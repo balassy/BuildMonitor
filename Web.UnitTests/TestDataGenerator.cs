09fa286c697ccf58067283f2eaaa7db3d4dfa0ce
György Balássy <balassy@users.noreply.github.com>
2018-09-22 01:54:45 +0200

    ADDED: Random test data generation with Bogus.

---
+++ Web.UnitTests/TestDataGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using BuildMonitor.Services.Interfaces;
+
+namespace BuildMonitor.Web.UnitTests
+{
+  internal static class TestDataGenerator
+  {
+    public static BuildResult GetBuildResult()
+    {
+      var result = new Faker<BuildResult>("en")
+        .RuleFor(r => r.BranchName, f => f.Lorem.Word())
+        .RuleFor(r => r.Id, f => f.Lorem.Word())
+        .RuleFor(r => r.Status, f => f.PickRandom<BuildStatus>())
+        .RuleFor(r => r.TriggeredBy, f => f.Person.FullName);
+      return result.Generate();
+    }
+
+    public static string GetDashboardId()
+    {
+      return new Faker().Lorem.Slug();
+    }
+  }
+}
