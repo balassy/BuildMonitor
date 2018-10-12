68c56bf67fa8c81973af8b13dce1e67a9581b4d8
György Balássy <balassy@users.noreply.github.com>
2018-10-12 22:41:14 +0200

    FIXED: The FooterComponent unit test is corrected by adding the DateFormatPipe to the declarations.

--- Web/ClientApp/src/app/footer/footer.component.spec.ts
+++ Web/ClientApp/src/app/footer/footer.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DateFormatPipe } from '../shared/pipes/date-format/date-format.pipe';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -9,7 +10,10 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ],
+      declarations: [
+        FooterComponent,
+        DateFormatPipe
+      ],
       imports: [ HttpClientModule ]
     })
     .compileComponents();
